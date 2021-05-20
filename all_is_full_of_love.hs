7ac7dcbad42c662ad8d8b42ac27aaa238ee19623
Tom Hoffman <tom.hoffman@gmail.com>
2021-05-20 11:40:48 -0400

    Finished staff 2.

--- all_is_full_of_love.hs
+++ all_is_full_of_love.hs
@@ -21,7 +21,7 @@ pads = [AcousticGrandPiano, BrightAcousticPiano, ElectricGrandPiano,
         HonkyTonkPiano, RhodesPiano, ChorusedPiano]
 
 bass_pads = take 2 pads
-rhythm_pads = take 3 (drop 2 pads)
+rhythm_pads = take 3 $ drop 2 pads
 lead_pad = drop 5 pads
 
 volumes = [127, 127, 96, 96, 96, 127, 127]
@@ -31,7 +31,7 @@ volumes = [127, 127, 96, 96, 96, 127, 127]
 zipMeasure :: [Music Pitch] -> Music (Pitch, Volume)
 -- Inputs a list of six pitches, assigns each to an instrument 
 -- and combines them in parallel.
-zipMeasure m = chord (zipWith addVolume volumes (zipWith instrument pads m))
+zipMeasure m = chord $ zipWith addVolume volumes $ zipWith instrument pads m
 
 c2df :: Pitch -> Pitch
 -- Fixes pitches entered from D flat sheet music without flats.
@@ -51,7 +51,7 @@ dfnote :: Dur -> Pitch -> Music Pitch
 dfnote d p = note d (c2df p)
 
 motif_line :: [Pitch] -> [Pitch] -> Music Pitch
-motif_line a b = line (map p2eighth (a ++ b))
+motif_line a b = line $ map p2eighth (a ++ b)
 
 -- Song constants
 
@@ -71,7 +71,8 @@ motif_b_bottom  = [(D, 4), (D, 4), (F, 4), (E, 4)]
 motif_ab_top    = motif_line motif_a_top motif_b_top
 motif_ab_bottom = motif_line motif_a_bottom motif_b_bottom
 
-
+ab_5_variation  = [cut (7 * en) motif_ab_bottom :+: dfnote en (C, 4),
+                   cut (7 * en) motif_ab_top]
 
 motif_c_top     = [(G, 5), (A, 5), (A, 5), (G, 5)]
 motif_c_bottom  = [(D, 5), (G, 4), (G, 4), (D, 5)]
@@ -82,17 +83,19 @@ motif_d_bottom  = [(G, 4), (G, 4), (D, 5), (G, 4)]
 motif_cd_top    = motif_line motif_c_top motif_d_top
 motif_cd_bottom = motif_line motif_d_bottom motif_d_bottom
 
+bass_d = [dfnote wn (D, 2), dfnote wn (A, 3)]
+bass_g = [dfnote wn (G, 2), dfnote wn (D, 3)]
+
+
 -- the song itself...
 
 staff_1 = times 4 (zipMeasure [wnr, wnr, motif_ab_bottom, 
                                motif_ab_top, wnr, wnr]) :+: addVolume 0 enr
 
--- measure 5 ends the motif on a different note and the bass begins
-measure_5 = [dfnote wn (F, 2), wnr, 
-             cut (7 * en) motif_ab_bottom :+: dfnote en (C, 4),
-             cut (7 * en) motif_ab_top, wnr,
+-- measure 5 uses a recurring variation on the ab motif
+measure_5 = [dfnote wn (F, 2), wnr] ++ ab_5_variation ++ [wnr,
              line [dfnote qn (F, 3), dfnote qn (A, 4), 
-                   dfnote en (B, 3), dfnote qn (D, 3), dfnote en (F, 3)]]
+             dfnote en (B, 3), dfnote qn (D, 3), dfnote en (F, 3)]]
 
 -- measure 6 has variation on motif a and different rhythm
 measure_6_rhythm :: [Pitch] -> Music Pitch
@@ -100,17 +103,19 @@ measure_6_rhythm l =
   let dur_6 = [qn, qn, en, dhn]
   in  line (zipWith dfnote dur_6 l)
 
-measure_6 = [dfnote wn (D, 2), dfnote wn (A, 3), 
-             measure_6_rhythm motif_a_bottom,
-             measure_6_rhythm [(D, 5), (F, 4), (F, 4), (D, 5)], wnr,
-             dfnote qn (F, 3)]
+measure_6 = bass_d ++ [measure_6_rhythm motif_a_bottom,
+            measure_6_rhythm [(D, 5), (F, 4), (F, 4), (D, 5)], wnr,
+            dfnote qn (F, 3)]
 
 -- measure 7 introduces new rhythm motif
-measure_7 = [dfnote wn (G, 2), dfnote wn (D, 3),
-             motif_cd_bottom, motif_cd_top, wnr,
-             line [qnr, times 2 (dfnote qn (D, 3)), dfnote qn (F, 3)]]
+measure_7 = bass_g ++ [motif_cd_bottom, motif_cd_top, wnr,
+            line [qnr, times 2 (dfnote qn (D, 3)), dfnote qn (F, 3)]]
 
-staff_2 = line (map zipMeasure [measure_5, measure_6])
+measure_8 = bass_g ++ [motif_cd_bottom, motif_cd_top, wnr,
+            line [dfnote en (F, 3), dfnote qn (E, 3), 
+                  times 2 (dfnote en (E, 3)), dfnote dqn (D, 3)]]
+
+staff_2 = line (map zipMeasure [measure_5, measure_6, measure_7, measure_8])
 
 staffs = [staff_1,
           staff_2]
